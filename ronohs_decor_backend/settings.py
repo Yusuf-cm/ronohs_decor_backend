b6c9913a1f3c9954d2f2c1c8a9d3eb4a21889c68
Yusuf-cm <yusufngetich7@gmail.com>
2025-09-04 03:48:51 +0300

    change

--- ronohs_decor_backend/settings.py
+++ ronohs_decor_backend/settings.py
@@ -74,10 +74,9 @@ WSGI_APPLICATION = "ronohs_decor_backend.wsgi.application"
 
 # Database
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL")
+    )
 }
 if IS_PRODUCTION:
     DATABASES["default"] = dj_database_url.config(conn_max_age=600, ssl_require=True)
